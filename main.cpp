6dd8bc71e046da5a14eba991c020caf7a26a846d
FluffyMaguro <uminomaguro@gmail.com>
2021-07-08 21:31:54 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <string>
+#include <vector>
+// #include <cstdlib>
+// #include <chrono>
+#include <random>
+
+#include "mutils.h" // adds "print" and "str" fuction templates
+
+//
+// PLAYER CLASS
+//
+class Player
+{
+public:
+    // Actual player skill
+    double skill;
+    // MMR assigned by the matchmaker
+    int mmr = 0;
+    // Vector of opponent skills. It's used to define the matchmaker accuracy.
+    std::vector<double> history;
+    // Define player and assign him his skill value
+    Player(double pskill)
+    {
+        skill = pskill;
+    };
+};
+
+//Overload my str fuction for printing the player
+std::string str(Player p)
+{
+    return str("Player(skill=", p.skill) + str(", mmr=", p.mmr) + str(", history=", p.history, ")");
+}
+
+// ABSTRACT CLASS FOR MATCHMAKING STRATEGY
+class MatchmakingStrategy
+{
+public:
+    virtual void get_chance(Player p1, Player p2) = 0;
+    virtual void update_mmr(Player p1, Player p2) = 0;
+};
+
+//
+// ELO MATCHMAKING STRATEGY
+//
+class ELO_strategy : public MatchmakingStrategy
+{
+public:
+    void get_chance(Player p1, Player p2)
+    {
+    }
+    void update_mmr(Player p1, Player p2)
+    {
+    }
+};
+
+//
+// SIMULATION CLASS
+//
+class Simulation
+{
+    std::default_random_engine generator;
+    std::normal_distribution<> skill_distribution;
+    MatchmakingStrategy *strategy;
+    std::vector<Player> players;
+
+public:
+    Simulation(MatchmakingStrategy &strat)
+    {
+        strategy = &strat;
+        // Initialize RNG generation for players
+        std::default_random_engine new_generator(333); //fixed seed for now
+        generator = new_generator;
+        std::normal_distribution<> new_skill_distribution(2, 0.4); // mean, std
+        skill_distribution = new_skill_distribution;
+    }
+
+    // Adds `number` of players to the simulation
+    void add_players(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            Player p(skill_distribution(generator));
+            players.push_back(p);
+            print("Adding:", p);
+        }
+    }
+
+    // Removes `number` of players from the simulation
+    void remove_players(int number)
+    {
+        for (int i = 0; i < number && players.size() > 0; i++)
+        {
+            players.pop_back();
+        }
+    }
+
+    // Runs the simulation for `number` of games
+    void play_games(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            print("playing a game...");
+        }
+    }
+
+    // Calculates inaccuracy of matchmaking (sum of square of skill differences / number of players)
+    // Accurate only for the same amount of games (as early games will be more inaccurate)
+    double get_inaccuracy()
+    {
+        double total = 0;
+        for (const Player &player : players)
+        {
+            for (const double &opponent : player.history)
+            {
+                double diff = opponent - player.skill;
+                total += diff * diff;
+            }
+        }
+        return total / players.size();
+    }
+};
+
+int main()
+{
+    ELO_strategy strategy;
+    Simulation simulation(strategy);
+    simulation.add_players(10);
+    simulation.play_games(10);
+    double inaccuracy = simulation.get_inaccuracy();
+    print("inaccuracy:", inaccuracy);
+}
---
+++ mutils.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+// Functions for creating a string from multiple variables and priting multiple variables
+// Doesn't work on arrays correctly (as they decay to pointers when passed to a function).
+// Templated functions need to have implementation passed in a header file as they are
+// created and compiled as needed by each CPP file. They do not conflict even with the same arguments.
+
+template <typename A>
+std::string str(A a)
+{
+    std::ostringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+template <typename T>
+std::string str(const std::vector<T> &a)
+{
+    if (a.size() == 0)
+    {
+        return "[]";
+    }
+
+    std::string s = "[";
+    for (const T &el : a)
+    {
+        s += str(el);
+        s += ", ";
+    }
+    s.erase(s.end() - 2, s.end());
+    s += "]";
+    return s;
+}
+
+template <typename A, typename B, typename C, typename D, typename E, typename F>
+std::string str(A a, B b, C c, D d, E e, F f)
+{
+    return str(a) + str(b) + str(c) + str(d) + str(e) + str(f);
+}
+
+template <typename A, typename B, typename C, typename D, typename E>
+std::string str(A a, B b, C c, D d, E e)
+{
+    return str(a) + str(b) + str(c) + str(d) + str(e);
+}
+
+template <typename A, typename B, typename C, typename D>
+std::string str(A a, B b, C c, D d)
+{
+    return str(a) + str(b) + str(c) + str(d);
+}
+
+template <typename A, typename B, typename C>
+std::string str(A a, B b, C c)
+{
+    return str(a) + str(b) + str(c);
+}
+
+template <typename A, typename B>
+std::string str(A a, B b)
+{
+    return str(a) + str(b);
+}
+
+template <typename A, typename B, typename C, typename D, typename E, typename F>
+void print(A a, B b, C c, D d, E e, F f)
+{
+    std::cout << str(a) << " " << str(b) << " " << str(c) << " " << str(d) << " "
+              << str(e) << " " << str(f) << "\n";
+}
+
+template <typename A, typename B, typename C, typename D, typename E>
+void print(A a, B b, C c, D d, E e)
+{
+    std::cout << str(a) << " " << str(b) << " " << str(c) << " " << str(d) << " "
+              << str(e) << "\n";
+}
+
+template <typename A, typename B, typename C, typename D>
+void print(A a, B b, C c, D d)
+{
+    std::cout << str(a) << " " << str(b) << " " << str(c) << " " << str(d)
+              << "\n";
+}
+
+template <typename A, typename B, typename C>
+void print(A a, B b, C c)
+{
+    std::cout << str(a) << " " << str(b) << " " << str(c)
+              << "\n";
+}
+
+template <typename A, typename B>
+void print(A a, B b)
+{
+    std::cout << str(a) << " " << str(b)
+              << "\n";
+}
+
+template <typename A>
+void print(A a)
+{
+    std::cout << str(a) << "\n";
+}
+
+// Logs something to the file
+template <typename A>
+void log(A a, std::string filename = "log.txt")
+{
+    std::string s = str(a);
+    std::cout << s << std::endl;
+    std::ofstream myfile(filename, std::ios::out | std::ios::app);
+    if (myfile.is_open())
+    {
+        myfile << s << std::endl;
+        myfile.close();
+    }
+    else
+    {
+        std::cout << "Can't save to the file. Won't open.\n";
+    }
+}
